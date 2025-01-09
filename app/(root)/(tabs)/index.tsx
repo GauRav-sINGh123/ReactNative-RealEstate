156a19c01b25e356fa07e21aed955615b7cc9e86
Gaurav Singh <gksingh947@gmail.com>
2025-01-09 16:44:04 +0530

    Style Configuration Complete

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import {  View } from "react-native";
+import {  View, Text } from "react-native";
 import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View>
+    <View className="flex-1 items-center justify-center">
+      <Text>Welcome To Real Esate App</Text>
       <Link href="/signin">Signin</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
