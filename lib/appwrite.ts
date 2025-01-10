4a4351736a06a5e0e6e6b1f1e00ca029b6ad302d
Gaurav Singh <gksingh947@gmail.com>
2025-01-10 16:58:50 +0530

    Appwrite Login Issue Fixed

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -9,7 +9,6 @@ export const config = {
 };
 
 export const client = new Client();
-
 client
   .setEndpoint(config.endpoint!)
   .setProject(config.projectId!)
@@ -17,27 +16,33 @@ client
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
-
+ 
 export async function login() {
   try {
     const redirectUri = Linking.createURL("/");
 
-    const response =await account.createOAuth2Token(OAuthProvider.Google,redirectUri)
-    if(!response) throw new Error("Authentication Failed")
+    const response = await account.createOAuth2Token(
+      OAuthProvider.Google,
+      redirectUri
+    );
+    if (!response) throw new Error("Create OAuth2 token failed");
 
-    const browserResult=await openAuthSessionAsync(response.toString(),redirectUri)
+    const browserResult = await openAuthSessionAsync(
+      response.toString(),
+      redirectUri
+    );
     if (browserResult.type !== "success")
-        throw new Error("Create OAuth2 token failed");
-  
-      const url = new URL(browserResult.url);
-      const secret = url.searchParams.get("secret")?.toString();
-      const userId = url.searchParams.get("userId")?.toString();
+      throw new Error("Create OAuth2 token failed");
 
-      if (!secret || !userId) throw new Error("Create OAuth2 token failed");
-  
-      const session = await account.createSession(userId, secret);
-      if (!session) throw new Error("Failed to create session");
+    const url = new URL(browserResult.url);
+    const secret = url.searchParams.get("secret")?.toString();
+    const userId = url.searchParams.get("userId")?.toString();
+    if (!secret || !userId) throw new Error("Create OAuth2 token failed");
 
+    const session = await account.createSession(userId, secret);
+    if (!session) throw new Error("Failed to create session");
+
+    return true;
   } catch (error) {
     console.error(error);
     return false;
@@ -45,30 +50,30 @@ export async function login() {
 }
 
 export async function logout() {
-    try {
-      const result = await account.deleteSession("current");
-      return result;
-    } catch (error) {
-      console.error(error);
-      return false;
-    }
+  try {
+    const result = await account.deleteSession("current");
+    return result;
+  } catch (error) {
+    console.error(error);
+    return false;
   }
+}
 
-  export async function getCurrentUser() {
-    try {
-      const result = await account.get();
-      if (result.$id) {
-        const userAvatar = avatar.getInitials(result.name);
-  
-        return {
-          ...result,
-          avatar: userAvatar.toString(),
-        };
-      }
-  
-      return null;
-    } catch (error) {
-      console.log(error);
-      return null;
+export async function getCurrentUser() {
+  try {
+    const result = await account.get();
+    if (result.$id) {
+      const userAvatar = avatar.getInitials(result.name);
+
+      return {
+        ...result,
+        avatar: userAvatar.toString(),
+      };
     }
+
+    return null;
+  } catch (error) {
+    console.log(error);
+    return null;
   }
+}
